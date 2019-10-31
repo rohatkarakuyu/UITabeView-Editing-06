563512da53de295dc03f8891c203a96dce7f70b6
deenmeefe <eddededeed>
2019-10-31 11:25:48 -0700

    Initial Commit

---
+++ Homeowner/ItemStore.swift
@@ -0,0 +1,39 @@
+//
+//  ItemStore.swift
+//  Homeowner
+//
+//  Created by Rohat Karakuyu on 30.10.2019.
+//  Copyright © 2019 Test. All rights reserved.
+//
+
+import UIKit
+
+class ItemStore {
+    
+    var allItems = [Item]()
+    
+    @discardableResult func createItem() -> Item {
+        let newItem = Item(random: true)
+        allItems.append(newItem)
+        return newItem
+    }
+    
+    func removeItem(_ item: Item) {
+        if let index = allItems.index(of: item) {
+            allItems.remove(at: index)
+        }
+    }
+    
+
+    func moveItem(from fromIndex: Int, to toIndex: Int) {
+        if fromIndex == toIndex {
+            return
+        }
+        let movedItem = allItems[fromIndex]
+        allItems.remove(at: fromIndex)
+        allItems.insert(movedItem, at: toIndex)
+    }
+ 
+    
+    
+}
---
+++ Homeowner/ItemsViewController.swift
@@ -0,0 +1,71 @@
+//
+//  ItemsViewController.swift
+//  Homeowner
+//
+//  Created by Rohat Karakuyu on 30.10.2019.
+//  Copyright © 2019 Test. All rights reserved.
+//
+
+import UIKit
+
+class ItemsViewController: UITableViewController{
+    
+    @IBAction func addNewItem(_ sender: UIButton) {
+
+        let newItem = itemStore.createItem()
+        if let index = itemStore.allItems.firstIndex(of: newItem) {
+            let indexPath = IndexPath(row: index, section: 0)
+            tableView.insertRows(at: [indexPath], with: .automatic)
+        }
+    }
+    
+    
+    @IBAction func toggleEditingMode(_ sender: UIButton) {
+        
+        if isEditing {
+            sender.setTitle("Edit", for: .normal)
+            setEditing(false, animated: true)
+        } else {
+            sender.setTitle("Done", for: .normal)
+            setEditing(true, animated: true)
+        }
+    }
+    
+    var itemStore: ItemStore!
+    
+    override func tableView(_ tableView: UITableView,
+                            numberOfRowsInSection section: Int) -> Int {
+        return itemStore.allItems.count
+    }
+    
+    override func tableView(_ tableView: UITableView,
+                            cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+
+        let cell = UITableViewCell(style: .value1, reuseIdentifier: "firatCell")
+
+        let item = itemStore.allItems[indexPath.row]
+        cell.textLabel?.text = item.name
+        cell.detailTextLabel?.text = "$\(item.valueInDollars)"
+        return cell
+    }
+    
+    override func tableView( _ tableView: UITableView,
+                             commit EditingStyle: UITableViewCell.EditingStyle,
+                            forRowAt indexPath: IndexPath) {
+        if  EditingStyle == .delete {
+            let item = itemStore.allItems[indexPath.row]
+            itemStore.removeItem(item)
+            tableView.deleteRows(at: [indexPath], with: .automatic)
+        }
+    }
+
+   
+    override func tableView(_ tableView: UITableView,
+                            moveRowAt sourceIndexPath: IndexPath,
+                            to destinationIndexPath: IndexPath) {
+        itemStore.moveItem(from: sourceIndexPath.row, to: destinationIndexPath.row)
+    }
+    
+    
+    
+}
